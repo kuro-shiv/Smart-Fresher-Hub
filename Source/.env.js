6ab34e7ba41c0a531b16d264111d388d35840ecc
Shivam Kumar Dubey <167622583+kuro-shiv@users.noreply.github.com>
2025-07-25 15:32:08 +0530

    Update .env.js

--- Source/.env.js
+++ Source/.env.js
@@ -1,9 +1,9 @@
 const ENV = 
 {
   SMTP_EMAIL: "ankitaffilate1806@gmail.com",
-  SMTP_PASS: "lyer kgtk xlyu ahhz", // App password from Google
+  SMTP_PASS: "", // App password from Google
   SHEET_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzAjCIi4rJ72nurPowajY6bVF1chGPjpNYUhHH9SAwHNvd_SS74ALL_fKAll8_4kAnylA/exec",
   SHEET1_NAME: "ResumeBuilder",
-  OPENROUTER_API_KEY: "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",  // your OpenRouter key
+  OPENROUTER_API_KEY: "sk-or-v1-",  // your OpenRouter key
   API_URL: "https://api.openrouter.com/v1/chat/completions"
 }
